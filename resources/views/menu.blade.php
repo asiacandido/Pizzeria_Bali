dd36448628fb060eb6d8589c9c8f55c274e9f804
asiacandido <asiacandido92@icloud.com>
2024-07-31 16:52:11 +0200

    menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,14 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<x-layout>
 
+<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top p-0">
+    <div class="container-fluid colorNav p-0 mb-0">
+      
+      <a class="navbar-brand" href="#"><img src="/img/logo3.png" alt="" height="200px"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="{{route('home')}}">Home</a>
+          </li>
+         
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('menu')}}">Menu</a>
+          </li>
+          
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contacts</a>
+          </li>
+        
+         
+        </ul>
+        
+      </div>
+    </div>
+  </nav>
+
+  <header>
+    <div class="headerImg ">
+        <img src="/img/header3.jpeg" alt="" class="imgHeader">
+        <h1 class="headerText pulse-animation text-center">Pizzeria & Restaurant</h1>
+        
+    </div>
     
-    
-</body>
-</html>
+</header>
+
+
+
+
+</x-layout>

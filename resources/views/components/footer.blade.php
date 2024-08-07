51ddaef1878f03d75bddfb8f92991a46f1f83f9c
asiacandido <asiacandido92@icloud.com>
2024-08-07 16:32:02 +0200

    menu pizze

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,114 @@
-<footer class="bg-body-tertiary text-center text-lg-start">
+{{-- <footer class="bg-body-tertiary text-center text-lg-start">
+
     <!-- Copyright -->
     <div class="text-center p-3 footer" style="background-color: #ECC273;">
       © 2024 Copyright:
       <a class="text-body">PugliaMia.com</a>
     </div>
     <!-- Copyright -->
-  </footer>
+  </footer> --}}
+
+
+
+
+
+<!-- Footer -->
+<footer class="text-center text-lg-start  text-muted" style="background-color: #ECC273;">
+  <!-- Section: Social media -->
+  
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Puglia Mia
+          </h6>
+          <p>
+            
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Location
+          </h6>
+          <p>
+            Gili Air
+          </p>
+         
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Opening Hours
+          </h6>
+          <p>
+           Monday - Sunday
+          </p>
+        
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contacts</h6>
+        
+            <i class="fas fa-envelope me-3"></i>
+            pugliamia@info.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color:  #ECC273;">
+    © 2024 Copyright:
+    <a class="text-reset fw-bold">Puglia Mia | All rights reserved</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -31,12 +31,86 @@
   <header>
     <div class="headerImg ">
         <img src="/img/header3.jpeg" alt="" class="imgHeader">
-        <h1 class="headerText pulse-animation text-center">Pizzeria & Restaurant</h1>
-        
+        <h1 class="headerText pulse-animation text-center ">Pizzeria & Restaurant</h1>
     </div>
-    
 </header>
 
+<div class="text-center mt-5 menu">SOURDOUGH PIZZA</div>
+ <div class="container-fluid">
+  <div class="row justify-content-center mt-5 ms-0 me-0">
+      <!-- Colonna 1 -->
+      <div class="col-12 col-md-3 custom-margin ">
+          <div class="row text-center">
+              <div class="col-12">
+                <h3 class="pizze">Margherita</h3>
+                <h4>tomato sauce, mozzarella, basil, extravirgin olive oil</h4>
+              </div>
+              
+              
+              <div class="col-12 mt-5">
+                <h3 class="pizze">Puglia Mia</h3>
+                <h4>mozzarella, smoked cured ham, fried aubergine, parmesan cheese, burratina on top - no tomato sauce</h4>
+              </div>
+              <div class="col-12 mt-5">
+                <h3 class="pizze">Prosciutto e Funghi</h3>
+                <h4>tomato sauce, mozzarella, smoked ham, mushrooms</h4>
+              </div>
+              <div class="col-12 mt-5">
+                <h3 class="pizze"></h3>
+              </div>
+              <div class="col-12">
+                <h3 class="pizze">Imperiale</h3>
+                <h4>tomato sauce, mozzarella, cured ham, parmesan cheese</h4>
+              </div>
+          </div>
+      </div>
+      <!-- Colonna 2 -->
+      <div class="col-12 col-md-3 custom-margin">
+          <div class="row text-center">
+            
+              <div class="col-12">
+                <h3 class="pizze">Marinara</h3>
+                <h4>tomato sauce, garlic, oregano, anchovies, extravirgin olive oil</h4>
+              </div>
+              <div class="col-12 mt-5">
+                <h3 class="pizze">Say "Cheese"</h3>
+                <h4>mozzarella, gorgonzola, taleggio, parmesan cheese</h4>
+              </div>
+              <div class="col-12 mt-5">
+                <h3 class="pizze">Parmigiana</h3>
+                <h4>tomato sauce, mozzarella, fried aubergine, parmesan, rucola</h4>
+              </div>
+              <div class="col-12 mt-5 ">
+                <h3 class="pizze">Calzone</h3>
+                <h4>tomato sauce, mozzarella, smoked ham, mushrooms</h4>
+              </div>
+              
+          </div>
+      </div>
+      <!-- Colonna 3 -->
+      <div class="col-12 col-md-3 custom-margin">
+          <div class="row text-center">
+              <div class="col-12">
+                <h3 class="pizze">Diavola</h3>
+                <h4>tomato sauce, mozzarella, spicy salami, oregano</h4>
+              </div>
+              <div class="col-12 mt-5">
+                <h3 class="pizze">Contadina</h3>
+                <h4>charry tomato sauce, fresh charry tomatoes, green pesto, shredded mozzarella to go, basil</h4>
+              </div>
+              <div class="col-12 mt-5">
+                <h3 class="pizze">Stay Healthy</h3>
+                <h4>tomato sauce, mozzarella, grilled zucchini, grilled aubergine, basil, extravirgin olive oil</h4>
+              </div>
+              <div class="col-12 mt-5">
+                <h3 class="pizze">Capricciosa</h3>
+                <h4>tomato sauce, mozzarella, mushrooms, artichokes, smoked ham, black olives</h4>
+              </div>
+              
+          </div>
+      </div>
+  </div>
+</div>
 
 
 

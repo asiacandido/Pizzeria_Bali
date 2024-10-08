c878cb0f78b9a3eeef92ddd8fe7fb3e05c0d6982
asiacandido <asiacandido92@icloud.com>
2024-10-08 21:49:40 +0200

    header

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -14,6 +14,8 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 
+
+
     
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 

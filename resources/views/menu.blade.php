cc02b4a93dd2a9ac3ee878a757e5a616204f2204
asiacandido <asiacandido92@icloud.com>
2025-05-15 21:14:18 +0200

    sistemata nav

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -4,36 +4,37 @@
     <div class="container-fluid colorNav p-0 mb-0">
       
       <a class="navbar-brand" href="#"><img src="/img/italianpizzeria.png" alt="" height="120px"></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+  
+      <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+        <!-- Links centrati -->
+        <ul class="navbar-nav nav-links mx-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link" aria-current="page" href="{{route('home')}}">About Us</a>
           </li>
-          
           <li class="nav-item">
             <a class="nav-link" href="{{route('menu')}}">Menu</a>
           </li>
-          
           <li class="nav-item">
             <a class="nav-link" href="#contact">Contacts</a>
           </li>
         </ul>
-        
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0"> <!-- New UL for icons -->
+  
+        <!-- Icona Instagram a destra -->
+        <ul class="navbar-nav ms-lg-auto d-flex flex-row align-items-center mb-2 mb-lg-0 icon-nav">
           <li class="nav-item">
-            <a href="https://www.instagram.com/pugliamiabali/"><i class="bi bi-instagram"></i></a>
+            <a href="https://www.instagram.com/pugliamia_pizzeria_giliair/">
+              <i class="bi bi-instagram"></i>
+            </a>
           </li>
-
-          {{-- <li class="nav-item">
-            <a href=""><i class="bi bi-geo-alt-fill"></i></a>
-          </li> --}}
         </ul>
       </div>
     </div>
-</nav>
+  </nav>
 
   
   <header>

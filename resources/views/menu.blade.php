847fb2348ea0a39ba4a230339224cf1c69b0111e
asiacandido <asiacandido92@icloud.com>
2025-06-30 14:06:37 +0800

    changed

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -57,7 +57,7 @@
           <div class="col-12">
             <h3 class="pizze">Bruschetta</h3>
             <h4>toasted pizza slices topped with cherry tomatoes, parmesan cheese, extravirgin olive oil, oregano</h4>
-            <h5 class="text-center mt-3">55k</h5>
+            <h5 class="text-center mt-3">60k</h5>
           </div>
           
           
@@ -71,7 +71,7 @@
             <h3 class="pizze">Pizzotti Puglia Mia</h3>
             <h4>fried dough square with cherry tomatoes, parmesan cheese, basil
             </h4>
-            <h5 class="text-center mt-3">50k</h5>
+            <h5 class="text-center mt-3">60k</h5>
           </div>
           
           
@@ -85,7 +85,7 @@
           <div class="col-12">
             <h3 class="pizze">Veggie Crunchers</h3>
             <h4>lightly battered and fried seasonal vegetables, crispy on the outside and tender on the inside</h4>
-            <h5 class="text-center mt-3">55k</h5>
+            <h5 class="text-center mt-3">60k</h5>
           </div>
           
           
@@ -94,8 +94,164 @@
     </div>
   </div>
   
+      <!-- nuovi starter  -->
+
+  <div class="container-fluid">
+    <div class="row justify-content-center mt-5 ms-0 me-0">
+      <!-- Colonna 1 -->
+      <div class="col-12 col-md-3 custom-margin ">
+        <div class="row text-center">
+          <div class="col-12">
+            <h3 class="pizze">Polpette della Nonna</h3>
+            <h4>Crispy fried meatballs served on a fresh bed of wild rocket and cherry tomatoes (halal)</h4>
+            <h5 class="text-center mt-3">75k</h5>
+          </div>
+          
+          
+        </div>
+      </div>
+      <!-- Colonna 2 -->
+      <div class="col-12 col-md-3 custom-margin">
+        <div class="row text-center">
+          
+          <div class="col-12">
+            <h3 class="pizze">Polpette della Nonna al sugo</h3>
+            <h4>homestyle meatballs simmered in rich tomato sauce and parmesan, served with warm pizza dough bread  (halal)
+            </h4>
+            <h5 class="text-center mt-3">75k</h5>
+          </div>
+          
+          
+          
+          
+        </div>
+      </div>
+      <!-- Colonna 3 -->
+      <div class="col-12 col-md-3 custom-margin">
+        <div class="row text-center">
+          <div class="col-12">
+            <h3 class="pizze">Crocchette di patate</h3>
+            <h4>Crispy potato croquettes filled with creamy mashed potatoes and savory smoked ham (no halal)</h4>
+            <h5 class="text-center mt-3">65k</h5>
+          </div>
+          
+          
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+ 
+
+  <div class="text-center mt-5 menu">HANDMADE PASTA</div>
+  <h4 class="text-center mt-3">Our handmade pasta is inspired by the timeless traditions of Puglia in Southern Italy. Each shape is crafted with care, following recipes passed down through generations ... a celebration of simple ingredients, family heritage, and the rich culinary soul of Italy. From our kitchen to your table, an authentic taste of 
+    <br>
+    <br>Orecchiette are a signature pasta from Puglia, shaped like little ears and handmade using traditional methods. Their curved surface holds sauces beautifully, offering a truly regional experience.
+
+    <br>
+    <br>
+    A southern classic, cavatelli are gently rolled pasta shells that embrace hearty sauces with every curve. Comforting, rustic, and full of authentic Italian flavour.
+  </h4>
   
+</div>
+
   
+  <div class="container-fluid">
+    <div class="row justify-content-center mt-5 ms-0 me-0">
+      <!-- Colonna 1 -->
+      <div class="col-12 col-md-3 custom-margin ">
+        <div class="row text-center">
+          <div class="col-12">
+            <h2 class="pasta mb-5">Le Orecchiette</h2>
+            <h3 class="pizze">Orecchiette al pomodoro</h3>
+            <h4>handmade pasta tossed in a rich tomato sauce finished with aged Parmigiano and fresh basil
+            </h4>
+            <h5 class="text-center mt-3">85k</h5>
+          </div>
+          
+          
+          <div class="col-12 mt-5">
+            <h3 class="pizze">Orecchiette con polpette</h3>
+            <h4>handmade pasta served in a hearty tomato sauce with traditional homemade meatballs and parmesan (halal)</h4>
+            <h5 class="text-center mt-3">105k</h5>
+          </div>
+          <div class="col-12 mt-5">
+            <h3 class="pizze">Orecchiette alla Pugliese</h3>
+            <h4>handmade pasta with sautéed cime di rapa (bitter leafy greens) anchovies, fresh chili, and toasted breadcrumbs </h4>
+            <h5 class="text-center mt-3">95k</h5>
+          </div>
+          <div class="col-12 mt-5">
+            <h3 class="pizze"></h3>
+          </div>
+          <div class="col-12">
+          
+          </div>
+
+          <div class="col-12 mt-5">
+        
+          </div>
+          
+        </div>
+      </div>
+      <!-- Colonna 2 -->
+      <div class="col-12 col-md-3 custom-margin">
+        <div class="row text-center">
+          
+          <div class="col-12">
+            <h2 class="pasta mb-5">I Cavatelli</h2>
+            <h3 class="pizze">Cavatelli gamberetti & zucchine</h3>
+            <h4>handmade pasta tossed with tender shrimp and sautéed zucchini, this dish offers a light, delicate balance of sea and garden flavors</h4>
+            <h5 class="text-center mt-3">110k</h5>
+          </div>
+          <div class="col-12 mt-5">
+            <h3 class="pizze">Cavatelli alla mediterranea</h3>
+            <h4>handmade made with fresh tuna, cherry tomatoes, and black olives. a rustic Mediterranean dish full of bold, coastal flavor</h4>
+            <h5 class="text-center mt-3">110k</h5>
+          </div>
+          <div class="col-12 mt-5">
+            <h3 class="pizze">Cavatelli calamari & broccoli</h3>
+            <h4>handmade cavatelli pasta tossed with tender squid, anchovies, and crispy golden breadcrumbs</h4>
+            <h5 class="text-center mt-3">115k</h5>
+          </div>
+          <div class="col-12 mt-5 ">
+            
+          </div> 
+          <div class="col-12 mt-5 ">
+          
+            
+          </div>
+          
+        </div>
+      </div>
+      <!-- Colonna 3 -->
+      <div class="col-12 col-md-3 custom-margin">
+        <div class="row text-center">
+          <div class="col-12">
+            <h2 class="pasta mb-5">Gli Gnocchi</h2>
+            <h3 class="pizze">Gnocchi al Pomodoro</h3>
+            <h4>handmade potato gnocchi served with a rich tomato sauce and topped with freshly grated Parmigiano
+            </h4>
+            <h5 class="text-center mt-3">85k</h5>
+          </div>
+          <div class="col-12 mt-5">
+            <h3 class="pizze">Gnocchi al Gorgonzola</h3>
+            <h4>handmade pillowy gnocchi in a creamy gorgonzola sauce, finished with crunchy toasted walnuts and parmesan</h4>
+            <h5 class="text-center mt-3">105k</h5>
+          </div>
+          
+          <div class="col-12 mt-5">
+            
+          </div>
+          {{-- <div class="col-12 mt-5">
+            <h3 class="pizze">Tonno e Cipolla</h3>
+            <h4>tomato sauce, mozzarella, mushrooms, artichokes, smoked ham, black olives</h4>
+          </div> --}} 
+          
+        </div>
+      </div>
+    </div>
+  </div>
   
   
   
@@ -113,19 +269,19 @@
             <h3 class="pizze">Marinara</h3>
             <h4>tomato sauce, garlic, oregano, anchovies, extravirgin olive oil, basil
             </h4>
-            <h5 class="text-center mt-3">60k</h5>
+            <h5 class="text-center mt-3">65k</h5>
           </div>
           
           
           <div class="col-12 mt-5">
             <h3 class="pizze">Parmigiana</h3>
             <h4>tomato sauce, mozzarella, fried aubergine, parmesan, wild rocket, extravirgin olive oil, basil</h4>
-            <h5 class="text-center mt-3">90k</h5>
+            <h5 class="text-center mt-3">95k</h5>
           </div>
           <div class="col-12 mt-5">
             <h3 class="pizze">Prosciutto e Funghi</h3>
             <h4>tomato sauce, mozzarella, smoked ham, mushrooms, extravirgin olive oil, basil (NO HALAL)</h4>
-            <h5 class="text-center mt-3">110k</h5>
+            <h5 class="text-center mt-3">115k</h5>
           </div>
           <div class="col-12 mt-5">
             <h3 class="pizze"></h3>
@@ -139,6 +295,16 @@
             </h4>
             <h5 class="text-center mt-3">100k</h5>
           </div>
+
+          <div class="col-12 mt-5">
+            <h3 class="pizze">Tonno e Cipolla
+            </h3>
+            <h4>tomato sauce, mozzarella, tuna chunky, onion, cappers, olive oil 
+               <br>
+              
+            </h4>
+            <h5 class="text-center mt-3">95k</h5>
+          </div>
           
         </div>
       </div>
@@ -149,28 +315,30 @@
           <div class="col-12">
             <h3 class="pizze">Margherita</h3>
             <h4>tomato sauce, mozzarella, extravirgin olive oil, basil</h4>
-            <h5 class="text-center mt-3">70k</h5>
+            <h5 class="text-center mt-3">75k</h5>
           </div>
           <div class="col-12 mt-5">
             <h3 class="pizze">Contadina</h3>
             <h4>tomato sauce, fresh cherry tomatoes, green pesto, mozzarella, extravirgin olive oil, basil</h4>
-            <h5 class="text-center mt-3">100k</h5>
+            <h5 class="text-center mt-3">105k</h5>
           </div>
           <div class="col-12 mt-5">
             <h3 class="pizze">Imperiale</h3>
             <h4>tomato sauce, mozzarella, smoked ham, parmesan flakes, wild rocket, cherry tomato, extravirgin olive oil (NO HALAL)</h4>
-            <h5 class="text-center mt-3">115k</h5>
+            <h5 class="text-center mt-3">120k</h5>
           </div>
-          {{-- <div class="col-12 mt-5 ">
-            <h3 class="pizze">Puglia Mia</h3>
-            <h4>mozzarella, fried aubergine, parmesan flakes, burratina on top , extravirgin olive oil, basil - no tomato sauce</h4>
-            <h5 class="text-center mt-3">115k</h5>
-          </div> --}}
-          {{-- <div class="col-12 mt-5 ">
-            <h3 class="pizze">Calzone</h3>
-            <h4>tomato sauce, mozzarella, smoked ham, mushrooms</h4>
+          <div class="col-12 mt-5 ">
+            <h3 class="pizze">Salame e Blue Cheese</h3>
+            <h4>tomato sauce mozzarella, blue cheese, spicy salami, olive oil, basil (no tomato sauce - no halal)</h4>
+            <h5 class="text-center mt-3">100k</h5>
+          </div> 
+          <div class="col-12 mt-5 ">
+            <h3 class="pizze">Pugliese</h3>
+            <h4>mozzarella, broccoli cream, anchovies, fresh  cherry tomatoes, black olives, olive oil (no tomato sauce)</h4>
+            <h5 class="text-center mt-3">95k</h5>
+            
           </div>
-           --}}
+          
         </div>
       </div>
       <!-- Colonna 3 -->
@@ -180,26 +348,26 @@
             <h3 class="pizze">Diavola</h3>
             <h4>tomato sauce, mozzarella, spicy salami, extravirgin olive oil, basil (NO HALAL)
             </h4>
-            <h5 class="text-center mt-3">80k</h5>
+            <h5 class="text-center mt-3">85k</h5>
           </div>
           <div class="col-12 mt-5">
             <h3 class="pizze">Stay Healthy</h3>
             <h4>mozzarella, grilled zucchini, grilled aubergine, cherry tomatoes, extravirgin olive oil , basil (with tomato sauce or without tomato sauce)</h4>
-            <h5 class="text-center mt-3">100k</h5>
+            <h5 class="text-center mt-3">105k</h5>
           </div>
           <div class="col-12 mt-5">
             <h3 class="pizze">Capricciosa</h3>
             <h4>tomato sauce, mozzarella, mushrooms, artichokes, smoked ham, black olives, extravirgin olive oil, basil
             </h4>
-            <h5 class="text-center mt-3">120k</h5>
+            <h5 class="text-center mt-3">125k</h5>
           </div>
           <div class="col-12 mt-5">
             <h3 class="pizze">Say "Cheese"</h3>
             <h4>mozzarella, gorgonzola, taleggio, parmesan cheese, basil - no tomato sauce (white pizza)</h4>
-            <h5 class="text-center mt-3">115k</h5>
+            <h5 class="text-center mt-3">120k</h5>
           </div>
           {{-- <div class="col-12 mt-5">
-            <h3 class="pizze">Capricciosa</h3>
+            <h3 class="pizze">Tonno e Cipolla</h3>
             <h4>tomato sauce, mozzarella, mushrooms, artichokes, smoked ham, black olives</h4>
           </div> --}} 
           
@@ -209,6 +377,7 @@
   </div>
 
 <h5 class="text-center mt-1 ">add your extra toppings +10k</h5>
+<h5 class="text-center mt-1 "></h5>
 <br>
 <br>
 <br>
@@ -249,7 +418,7 @@
           <div class="col-12">
             <h3 class="pizze">Pizza with Nutella</h3>
             <h4>a sweet twist on the classic pizza, featuring a crispy pizza crust topped generously with Nutella, perfect for dessert lovers.</h4>
-            <h5 class="text-center mt-3">60k</h5>
+            <h5 class="text-center mt-3">65k</h5>
           </div>
           
           
@@ -279,6 +448,8 @@
           
         </div>
       </div>
+
+      
       <!-- Colonna 2 -->
       <div class="col-12 col-md-3 custom-margin">
         <div class="row text-center">
@@ -294,16 +465,7 @@
         </div>
       </div>
       <!-- Colonna 3 -->
-      <div class="col-12 col-md-3 custom-margin">
-        <div class="row text-center">
-          <div class="col-12">
-            <h3 class="pizze">Bintang Beer</h3>
-            <h5 class="text-center mt-3">40k</h5>
-          </div>
-          
-          
-        </div>
-      </div>
+     
     </div>
 
 
@@ -337,7 +499,72 @@
     
     </div>
 
+    <div class="text-center mt-5 menu">ALCOHOLIC BEVERAGES</div>
+    <div class="container-fluid">
+      <div class="row justify-content-center mt-5 ms-0 me-0">
+        <!-- Colonna 1 -->
+        <div class="col-12 col-md-3 custom-margin ">
+          <div class="row text-center">
+            <div class="col-12">
+              <h3 class="pizze">Bintang Beer</h3>
+              
+              <h5 class="text-center mt-3">40k</h5>
+            </div>
+            
+            
+          </div>
+          
+        </div>
 
+        <div class="col-12 col-md-3 custom-margin ">
+          <div class="row text-center">
+            <div class="col-12">
+              <h3 class="pizze">Aperol or Campari Spritz</h3>
+              
+              <h5 class="text-center mt-3">85k</h5>
+            </div>
+            
+            
+          </div>
+        </div>
+  
+        
+        <!-- Colonna 2 -->
+        <div class="col-12 col-md-3 custom-margin">
+          <div class="row text-center">
+            
+            <div class="col-12">
+              <h3 class="pizze">Local White Wine By Glass</h3>
+              <h5 class="text-center mt-3">55k</h5>
+            </div>
+            
+            
+            
+            
+          </div>
+        </div>
+        <!-- Colonna 3 -->
+        <div class="col-12 col-md-3 custom-margin">
+          <div class="row text-center">
+            <div class="col-12">
+              <h3 class="pizze">Local Red Wine By Glass</h3>
+              <h5 class="text-center mt-3">55k</h5>
+            </div>
+            
+            
+          </div>
+        </div>
+
+        
+
+        
+      </div>
+  
+  
+  
+  
+    
+    </div>
 
 
   
